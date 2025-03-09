97e81c0d7f8ea37320d5da7285eec5bae146225c
KevinNguyen <nhatkhuong86@gmail.com>
2025-03-09 23:30:47 +0700

    build 2

--- assets/main/index.js
+++ assets/main/index.js
@@ -1204,7 +1204,7 @@ System.register("chunks:///_virtual/BlockPuzzleScene.ts", ['./rollupPluginModLoB
   };
 });
 
-System.register("chunks:///_virtual/BoardController.ts", ['./rollupPluginModLoBabelHelpers.js', 'cc', './Utils2.ts', './BlockDragControl.ts', './Block.ts', './BlockPuzzleScene.ts'], function (exports) {
+System.register("chunks:///_virtual/BoardController.ts", ['./rollupPluginModLoBabelHelpers.js', 'cc', './Utils.ts', './BlockDragControl.ts', './Block.ts', './BlockPuzzleScene.ts'], function (exports) {
   var _applyDecoratedDescriptor, _inheritsLoose, _initializerDefineProperty, _assertThisInitialized, cclegacy, _decorator, Node, Prefab, CCInteger, Graphics, UITransform, Size, instantiate, Vec3, Color, Vec2, Component, Utils, BlockDragControl, Block, BlockPuzzleScene;
   return {
     setters: [function (module) {
@@ -2513,7 +2513,7 @@ System.register("chunks:///_virtual/LogManager.ts", ['cc'], function (exports) {
   };
 });
 
-System.register("chunks:///_virtual/main", ['./debug-view-runtime-control.ts', './telegram-web.ts', './AudioController.ts', './CanvasHelpper.ts', './ResizeWindowHelper.ts', './BasePopup.ts', './PopupAnimation.ts', './PopupController.ts', './PortalController.ts', './BaseScene.ts', './SceneController.ts', './LoadingGameScene.ts', './ActionHelper.ts', './ActionHelpper.ts', './Utilities.ts', './utils.ts', './BlockPuzzleScene.ts', './GameClosePopup.ts', './GameWinPopup.ts', './PromptPopup.ts', './AudioManager.ts', './Block.ts', './BlockDrag.ts', './BlockDragControl.ts', './BoardController.ts', './BlockData.ts', './User.ts', './PageBlock.ts', './SpineController.ts', './Utils2.ts', './Config.ts', './Events.ts', './GlobalEvent.ts', './LogManager.ts', './WalletController.ts', './WalletView.ts'], function () {
+System.register("chunks:///_virtual/main", ['./debug-view-runtime-control.ts', './telegram-web.ts', './AudioController.ts', './CanvasHelpper.ts', './ResizeWindowHelper.ts', './BasePopup.ts', './PopupAnimation.ts', './PopupController.ts', './PortalController.ts', './BaseScene.ts', './SceneController.ts', './LoadingGameScene.ts', './ActionHelper.ts', './ActionHelpper.ts', './Utilities.ts', './utils2.ts', './BlockPuzzleScene.ts', './GameClosePopup.ts', './GameWinPopup.ts', './PromptPopup.ts', './AudioManager.ts', './Block.ts', './BlockDrag.ts', './BlockDragControl.ts', './BoardController.ts', './BlockData.ts', './User.ts', './PageBlock.ts', './SpineController.ts', './Utils.ts', './Config.ts', './Events.ts', './GlobalEvent.ts', './LogManager.ts', './WalletController.ts', './WalletView.ts'], function () {
   return {
     setters: [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
     execute: function () {}
@@ -3513,7 +3513,7 @@ System.register("chunks:///_virtual/telegram-web.ts", ['./rollupPluginModLoBabel
   };
 });
 
-System.register("chunks:///_virtual/User.ts", ['cc', './Utils2.ts'], function (exports) {
+System.register("chunks:///_virtual/User.ts", ['cc', './Utils.ts'], function (exports) {
   var cclegacy, _decorator, Utils;
   return {
     setters: [function (module) {
@@ -3678,7 +3678,87 @@ System.register("chunks:///_virtual/Utilities.ts", ['cc'], function (exports) {
   };
 });
 
-System.register("chunks:///_virtual/utils.ts", ['cc'], function (exports) {
+System.register("chunks:///_virtual/Utils.ts", ['cc'], function (exports) {
+  var cclegacy, Vec3, resources, Material, MeshRenderer, sys;
+  return {
+    setters: [function (module) {
+      cclegacy = module.cclegacy;
+      Vec3 = module.Vec3;
+      resources = module.resources;
+      Material = module.Material;
+      MeshRenderer = module.MeshRenderer;
+      sys = module.sys;
+    }],
+    execute: function () {
+      cclegacy._RF.push({}, "c281cTqZQxLPqUJ4LIPsyO8", "Utils", undefined);
+      var Utils = exports('Utils', /*#__PURE__*/function () {
+        function Utils() {}
+        /** Convert position according to row and column */
+        Utils.convertRowColToPos = function convertRowColToPos(row, col, size, startX, startY) {
+          var x = startX + col * size + size / 2;
+          var y = startY + row * size + size / 2;
+          // console.log("x:"+ x +" :y:"+y);
+          // console.log("startX:"+ startX +" :startY:"+startY);
+          // console.log("==========size:"+ size +" :===============");
+          return new Vec3(x, y, 0);
+        }
+
+        /** Convert rows and columns based on position */;
+        Utils.convertPosToRowCol = function convertPosToRowCol(pos, size, startX, startY) {
+          var row = Math.round((pos.y - startY - size / 2) / size);
+          var col = Math.round((pos.x - startX - size / 2) / size);
+          return [row, col];
+        }
+
+        /** Set material */;
+        Utils.setMaterial = function setMaterial(meshNode, path) {
+          if (meshNode) {
+            resources.load(path, Material, function (err, material) {
+              // console.log(err, material);
+              meshNode.getComponent(MeshRenderer).material = material;
+            });
+          }
+        }
+
+        /**
+          * Set local data
+          * @param key 
+          * @param data 
+          */;
+        Utils.setLocalStorage = function setLocalStorage(key, data) {
+          try {
+            sys.localStorage.setItem(key, JSON.stringify(data));
+            return true;
+          } catch (e) {
+            console.error(e);
+          }
+          return false;
+        }
+
+        /**
+         * Get local data
+         * @param key 
+         */;
+        Utils.getLocalStorage = function getLocalStorage(key) {
+          try {
+            var dataStr = sys.localStorage.getItem(key);
+            if (dataStr) {
+              var data = JSON.parse(dataStr);
+              return data;
+            }
+          } catch (e) {
+            console.error(e);
+          }
+          return null;
+        };
+        return Utils;
+      }());
+      cclegacy._RF.pop();
+    }
+  };
+});
+
+System.register("chunks:///_virtual/utils2.ts", ['cc'], function (exports) {
   var cclegacy, Vec3, UITransform, tween, v3;
   return {
     setters: [function (module) {
@@ -3993,86 +4073,6 @@ System.register("chunks:///_virtual/utils.ts", ['cc'], function (exports) {
         for (e = 0; e < f.length; e += 16) q = a, r = b, s = c, t = d, a = k(a, b, c, d, f[e + 0], 7, 3614090360), d = k(d, a, b, c, f[e + 1], 12, 3905402710), c = k(c, d, a, b, f[e + 2], 17, 606105819), b = k(b, c, d, a, f[e + 3], 22, 3250441966), a = k(a, b, c, d, f[e + 4], 7, 4118548399), d = k(d, a, b, c, f[e + 5], 12, 1200080426), c = k(c, d, a, b, f[e + 6], 17, 2821735955), b = k(b, c, d, a, f[e + 7], 22, 4249261313), a = k(a, b, c, d, f[e + 8], 7, 1770035416), d = k(d, a, b, c, f[e + 9], 12, 2336552879), c = k(c, d, a, b, f[e + 10], 17, 4294925233), b = k(b, c, d, a, f[e + 11], 22, 2304563134), a = k(a, b, c, d, f[e + 12], 7, 1804603682), d = k(d, a, b, c, f[e + 13], 12, 4254626195), c = k(c, d, a, b, f[e + 14], 17, 2792965006), b = k(b, c, d, a, f[e + 15], 22, 1236535329), a = l(a, b, c, d, f[e + 1], 5, 4129170786), d = l(d, a, b, c, f[e + 6], 9, 3225465664), c = l(c, d, a, b, f[e + 11], 14, 643717713), b = l(b, c, d, a, f[e + 0], 20, 3921069994), a = l(a, b, c, d, f[e + 5], 5, 3593408605), d = l(d, a, b, c, f[e + 10], 9, 38016083), c = l(c, d, a, b, f[e + 15], 14, 3634488961), b = l(b, c, d, a, f[e + 4], 20, 3889429448), a = l(a, b, c, d, f[e + 9], 5, 568446438), d = l(d, a, b, c, f[e + 14], 9, 3275163606), c = l(c, d, a, b, f[e + 3], 14, 4107603335), b = l(b, c, d, a, f[e + 8], 20, 1163531501), a = l(a, b, c, d, f[e + 13], 5, 2850285829), d = l(d, a, b, c, f[e + 2], 9, 4243563512), c = l(c, d, a, b, f[e + 7], 14, 1735328473), b = l(b, c, d, a, f[e + 12], 20, 2368359562), a = m(a, b, c, d, f[e + 5], 4, 4294588738), d = m(d, a, b, c, f[e + 8], 11, 2272392833), c = m(c, d, a, b, f[e + 11], 16, 1839030562), b = m(b, c, d, a, f[e + 14], 23, 4259657740), a = m(a, b, c, d, f[e + 1], 4, 2763975236), d = m(d, a, b, c, f[e + 4], 11, 1272893353), c = m(c, d, a, b, f[e + 7], 16, 4139469664), b = m(b, c, d, a, f[e + 10], 23, 3200236656), a = m(a, b, c, d, f[e + 13], 4, 681279174), d = m(d, a, b, c, f[e + 0], 11, 3936430074), c = m(c, d, a, b, f[e + 3], 16, 3572445317), b = m(b, c, d, a, f[e + 6], 23, 76029189), a = m(a, b, c, d, f[e + 9], 4, 3654602809), d = m(d, a, b, c, f[e + 12], 11, 3873151461), c = m(c, d, a, b, f[e + 15], 16, 530742520), b = m(b, c, d, a, f[e + 2], 23, 3299628645), a = n(a, b, c, d, f[e + 0], 6, 4096336452), d = n(d, a, b, c, f[e + 7], 10, 1126891415), c = n(c, d, a, b, f[e + 14], 15, 2878612391), b = n(b, c, d, a, f[e + 5], 21, 4237533241), a = n(a, b, c, d, f[e + 12], 6, 1700485571), d = n(d, a, b, c, f[e + 3], 10, 2399980690), c = n(c, d, a, b, f[e + 10], 15, 4293915773), b = n(b, c, d, a, f[e + 1], 21, 2240044497), a = n(a, b, c, d, f[e + 8], 6, 1873313359), d = n(d, a, b, c, f[e + 15], 10, 4264355552), c = n(c, d, a, b, f[e + 6], 15, 2734768916), b = n(b, c, d, a, f[e + 13], 21, 1309151649), a = n(a, b, c, d, f[e + 4], 6, 4149444226), d = n(d, a, b, c, f[e + 11], 10, 3174756917), c = n(c, d, a, b, f[e + 2], 15, 718787259), b = n(b, c, d, a, f[e + 9], 21, 3951481745), a = h(a, q), b = h(b, r), c = h(c, s), d = h(d, t);
         return (p(a) + p(b) + p(c) + p(d)).toLowerCase();
       }
-      cclegacy._RF.pop();
-    }
-  };
-});
-
-System.register("chunks:///_virtual/Utils2.ts", ['cc'], function (exports) {
-  var cclegacy, Vec3, resources, Material, MeshRenderer, sys;
-  return {
-    setters: [function (module) {
-      cclegacy = module.cclegacy;
-      Vec3 = module.Vec3;
-      resources = module.resources;
-      Material = module.Material;
-      MeshRenderer = module.MeshRenderer;
-      sys = module.sys;
-    }],
-    execute: function () {
-      cclegacy._RF.push({}, "c281cTqZQxLPqUJ4LIPsyO8", "Utils", undefined);
-      var Utils = exports('Utils', /*#__PURE__*/function () {
-        function Utils() {}
-        /** Convert position according to row and column */
-        Utils.convertRowColToPos = function convertRowColToPos(row, col, size, startX, startY) {
-          var x = startX + col * size + size / 2;
-          var y = startY + row * size + size / 2;
-          // console.log("x:"+ x +" :y:"+y);
-          // console.log("startX:"+ startX +" :startY:"+startY);
-          // console.log("==========size:"+ size +" :===============");
-          return new Vec3(x, y, 0);
-        }
-
-        /** Convert rows and columns based on position */;
-        Utils.convertPosToRowCol = function convertPosToRowCol(pos, size, startX, startY) {
-          var row = Math.round((pos.y - startY - size / 2) / size);
-          var col = Math.round((pos.x - startX - size / 2) / size);
-          return [row, col];
-        }
-
-        /** Set material */;
-        Utils.setMaterial = function setMaterial(meshNode, path) {
-          if (meshNode) {
-            resources.load(path, Material, function (err, material) {
-              // console.log(err, material);
-              meshNode.getComponent(MeshRenderer).material = material;
-            });
-          }
-        }
-
-        /**
-          * Set local data
-          * @param key 
-          * @param data 
-          */;
-        Utils.setLocalStorage = function setLocalStorage(key, data) {
-          try {
-            sys.localStorage.setItem(key, JSON.stringify(data));
-            return true;
-          } catch (e) {
-            console.error(e);
-          }
-          return false;
-        }
-
-        /**
-         * Get local data
-         * @param key 
-         */;
-        Utils.getLocalStorage = function getLocalStorage(key) {
-          try {
-            var dataStr = sys.localStorage.getItem(key);
-            if (dataStr) {
-              var data = JSON.parse(dataStr);
-              return data;
-            }
-          } catch (e) {
-            console.error(e);
-          }
-          return null;
-        };
-        return Utils;
-      }());
       cclegacy._RF.pop();
     }
   };
